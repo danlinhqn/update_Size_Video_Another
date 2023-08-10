24abd7b20a4b018bfb70852c8b9d69be0fb7b450
Linh Tran <linh.tran@weallnet.com>
2023-08-10 13:02:23 +0700

    ok

--- function.py
+++ function.py
@@ -27,7 +27,7 @@ import pydirectinput
 import string
 from pywinauto import Application
 import psycopg2
-
+import cv2
 
 # Tại đây khai báo để gọi API sửa dụng upload DB UAT
 API_TOKEN = 'https://wapi.weallnet.com/api/TOKEN_AccessToken/GetClientAccessToken'
@@ -64,60 +64,21 @@ def getToken():
 
 TOKEN = getToken()
 
-# Hàm update lại Code random cho DB Photos
-def Update_Code_Random_DB():
+# Kết nối với DB | Kiểm tra Data sử dụng trước khi chạy chương trình
+def connectDB():
     
-    list_ID_Photos = []
+    conn = psycopg2.connect(
+        
+            # Data PROD
+            host="172.16.33.100",
+            port="5432",
+            database="WAN_Data",
+            user="wan_data",
+            password="fbpSk9MPmjheVzEtR8Ax6Q4NWYa3JnqG"
+
+        )
     
-    # Kết nối với DB | Kiểm tra Data sử dụng trước khi chạy chương trình
-    def connectDB():
-        
-        conn = psycopg2.connect(
-            
-                # Data PROD
-                host="172.16.33.100",
-                port="5432",
-                database="WAN_Data",
-                user="wan_data",
-                password="fbpSk9MPmjheVzEtR8Ax6Q4NWYa3JnqG"
-
-            )
-        
-        return(conn)
-
-    def getIDFromDB():
-
-        cur = connectDB().cursor()
-
-        # Hàm hỗ trợ lấy dữ liệu ChannelConfigID + ID video tiktok + thời gian tải lên
-        
-        cur.execute( 'SELECT "PhotoID" FROM "Photos" ')
-
-        rows = cur.fetchall()
-
-        # Lưu vào Data => Chuẩn bị tới bước tiếp theo phân loại và xóa trash
-        for row in rows: 
-            list_ID_Photos.append(row[0])
-
-        # Ngắt kết nối khi lấy dữ liệu xong
-
-        cur.close()
-        connectDB().close()
-        
-    # ------------------------------------ /
-    # Hàm update lại Code random cho DB Photos
-    def updateCodeForPhotoID(photo_id):
-        # Tạo kết nối đến cơ sở dữ liệu
-        connection = connectDB()
-        cur = connection.cursor()
-
-        # Thực hiện lệnh SQL để cập nhật cột "Code"
-        cur.execute('UPDATE "Photos" SET "Code" = %s WHERE "PhotoID" = %s', (str(uuid.uuid1()), photo_id))
-
-        # Commit thay đổi vào cơ sở dữ liệu và đóng kết nối
-        connection.commit()
-        cur.close()
-        connection.close()
+    return(conn)
 
 # Hàm ghi giá trị txt vào file 
 def write_Value_To_File_Txt(file_path, values_to_append):
@@ -147,5 +108,86 @@ def get_image_dimensions_from_url(image_url):
     except Exception as e:
         print("Error:", str(e))
         return None, None
+
+# Hàm chính -------------------------------------- /
+def update_Width_Height_Video():
     
+    # Các bước sẽ thực hiện 
+        # Bước 1: Lấy ID video theo vòng lặp từ lớn đến nhỏ, có khung Weight trống
+        # Bước 2: Lưu đè giá trị vào file txt
+        # Bước 3: Tiến hành vòng lặp để lấy VideoScreenShot có trong DB, để xác định kích thước
     
+    # --------------------------------------- /
+
+    # Hàm lấy width, height của video
+    def get_video_dimensions(video_url):
+        
+        try:
+            cap = cv2.VideoCapture(video_url)
+            width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            return width, height
+
+        except Exception as e: print("Error:", e)
+        
+        return None, None
+
+    # Hàm lấy các ID video từ DB
+    def get_Information_VideoID_New():
+        
+            Data_Save = [[],[],[]]
+
+            cur = connectDB().cursor()
+
+            # Hàm hỗ trợ lấy dữ liệu ChannelConfigID + ID video tiktok + thời gian tải lên
+            
+            cur.execute( 'SELECT "VideoID", "RawPlayURL" FROM "Videos" WHERE "Height" IS NULL ORDER BY "VideoID" DESC LIMIT 1' )
+
+            rows = cur.fetchall()
+
+            # Lưu vào Data => Chuẩn bị tới bước tiếp theo phân loại và xóa trash
+            for row in rows: 
+                
+                Data_Save[0].append(row[0])
+                
+                # Lấy width, height của video
+                height, width = get_video_dimensions(row[1])
+                Data_Save[1].append(height)
+                Data_Save[2].append(width)
+
+            # Ngắt kết nối khi lấy dữ liệu xong
+
+            cur.close()
+            connectDB().close()
+            
+            return Data_Save
+    
+    # Hàm update lại Code random cho DB Photos
+    def Update_Code_Random_DB(Height ,Width, VideoID):
+        
+        if int(Height) > int(Width): IsVertical = True
+        else: IsVertical = False
+        Width = int(Width)
+        Height = int(Height)
+        VideoID = int(VideoID)
+        # Tạo kết nối đến cơ sở dữ liệu
+        connection = connectDB()
+        cur = connection.cursor()
+
+        # Thực hiện lệnh SQL để cập nhật cột "Code"
+    
+        cur.execute('UPDATE "Videos" SET "Height" = %s, "Width" = %s , "IsVertical" = %s Where "VideoID" = %s ', (int(Height), int(Width), IsVertical, VideoID))
+
+
+        # Commit thay đổi vào cơ sở dữ liệu và đóng kết nối
+        connection.commit()
+        cur.close()
+        connection.close()
+    
+    # Chạy test
+    data_Update_DB = get_Information_VideoID_New()
+    Update_Code_Random_DB(data_Update_DB[2][0], data_Update_DB[1][0], data_Update_DB[0][0])
+    print('/-- Update for VideoID: ', data_Update_DB[0][0], ' --/')
+
+
+
--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 from function import *
+
+while 1: update_Width_Height_Video()
