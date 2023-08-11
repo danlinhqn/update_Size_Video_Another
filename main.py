a1981d33f7ee9de1dc751ce32ba9254b50edc2eb
Linh Tran <linh.tran@weallnet.com>
2023-08-11 14:26:13 +0700

    ok

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from function import *
 
-while 1: update_Width_Height_Video()
+while 1: 
+    
+    update_Width_Height_Video()
+    if value_Break_While[0] == 1: 
+        print('Done ...')
+        break 
